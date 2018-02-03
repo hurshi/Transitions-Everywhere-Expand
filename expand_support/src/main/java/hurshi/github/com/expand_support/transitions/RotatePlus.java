070911ac0b76aec0b7cde328526a3dd94a142544
化十 <e_Gavin@163.com>
2018-02-03 11:40:16 +0800

    add transition expand for android support lib

---
+++ expand_support/src/main/java/hurshi/github/com/expand_support/transitions/RotatePlus.java
@@ -0,0 +1,50 @@
+package hurshi.github.com.expand_support.transitions;
+
+import android.animation.Animator;
+import android.support.transition.TransitionValues;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by hurshi on 2017/12/19.
+ */
+
+public class RotatePlus extends Rotate {
+
+    private float pivotXPercent = 0.5f;
+    private float pivotYPercent = 0.5f;
+
+    public RotatePlus() {
+
+    }
+
+    public RotatePlus(float pivotXPercent, float pivotYPercent) {
+        super();
+        setPivotXPercent(pivotXPercent);
+        setPivotYPercent(pivotYPercent);
+    }
+
+    public RotatePlus setPivotXPercent(float value) {
+        if (value < 0f) {
+            throw new IllegalArgumentException("pivotXPercent cannot be negative!");
+        }
+        pivotXPercent = value;
+        return this;
+    }
+
+    public RotatePlus setPivotYPercent(float value) {
+        if (value < 0f) {
+            throw new IllegalArgumentException("pivotYPercent cannot be negative!");
+        }
+        pivotYPercent = value;
+        return this;
+    }
+
+
+    @Override
+    public Animator createAnimator(ViewGroup sceneRoot, TransitionValues startValues, TransitionValues endValues) {
+        endValues.view.setPivotX(endValues.view.getWidth() * pivotXPercent);
+        endValues.view.setPivotY(endValues.view.getHeight() * pivotYPercent);
+        return super.createAnimator(sceneRoot, startValues, endValues);
+    }
+}
---
+++ expand_support/src/main/java/hurshi/github/com/expand_support/transitions/ScalePlus.java
@@ -0,0 +1,147 @@
+package hurshi.github.com.expand_support.transitions;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.support.annotation.Nullable;
+import android.support.transition.Transition;
+import android.support.transition.TransitionListenerAdapter;
+import android.support.transition.TransitionValues;
+import android.support.transition.Visibility;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hurshi.github.com.expand_support.utils.TransitionUtils;
+
+/**
+ * Created by hurshi on 2018/2/3.
+ */
+
+public class ScalePlus extends Visibility {
+    private boolean scaleHorizontalEnable = true;
+    private boolean scaleVerticalEnable = true;
+    private float pivotXPercent = 0.5f;
+    private float pivotYPercent = 0.5f;
+
+    private static final String PROPNAME_SCALE_X = "scale:scaleX";
+    private static final String PROPNAME_SCALE_Y = "scale:scaleY";
+
+    private float mDisappearedScale = 0f;
+
+    public ScalePlus() {
+    }
+
+    public ScalePlus(float disappearedScale) {
+        setDisappearedScale(disappearedScale);
+    }
+
+    public ScalePlus setDisappearedScale(float disappearedScale) {
+        if (disappearedScale < 0f) {
+            throw new IllegalArgumentException("disappearedScale cannot be negative!");
+        }
+        mDisappearedScale = disappearedScale;
+        return this;
+    }
+
+    public ScalePlus setPivotXPercent(float value) {
+        if (value < 0f) {
+            throw new IllegalArgumentException("pivotXPercent cannot be negative!");
+        }
+        pivotXPercent = value;
+        return this;
+    }
+
+    public ScalePlus setPivotYPercent(float value) {
+        if (value < 0f) {
+            throw new IllegalArgumentException("pivotYPercent cannot be negative!");
+        }
+        pivotYPercent = value;
+        return this;
+    }
+
+    public ScalePlus setScaleHorizontalEnable(boolean enable) {
+        scaleHorizontalEnable = enable;
+        return this;
+    }
+
+    public ScalePlus setScaleVerticalEnable(boolean enable) {
+        scaleVerticalEnable = enable;
+        return this;
+    }
+
+    @Override
+    public void captureStartValues(TransitionValues transitionValues) {
+        super.captureStartValues(transitionValues);
+        if (transitionValues.view != null) {
+            transitionValues.values.put(PROPNAME_SCALE_X, transitionValues.view.getScaleX());
+            transitionValues.values.put(PROPNAME_SCALE_Y, transitionValues.view.getScaleY());
+        }
+    }
+
+
+    @Nullable
+    private Animator createAnimation(final View view, float startScale, float endScale, TransitionValues values) {
+        final float initialScaleX = view.getScaleX();
+        final float initialScaleY = view.getScaleY();
+        float startScaleX = initialScaleX * startScale;
+        float endScaleX = initialScaleX * endScale;
+        float startScaleY = initialScaleY * startScale;
+        float endScaleY = initialScaleY * endScale;
+        view.setPivotX(view.getWidth() * pivotXPercent);
+        view.setPivotY(view.getHeight() * pivotYPercent);
+
+        if (values != null) {
+            Float savedScaleX = (Float) values.values.get(PROPNAME_SCALE_X);
+            Float savedScaleY = (Float) values.values.get(PROPNAME_SCALE_Y);
+            // if saved value is not equal initial value it means that previous
+            // transition was interrupted and in the onTransitionEnd
+            // we've applied endScale. we should apply proper value to
+            // continue animation from the interrupted state
+            if (savedScaleX != null && savedScaleX != initialScaleX) {
+                startScaleX = savedScaleX;
+            }
+            if (savedScaleY != null && savedScaleY != initialScaleY) {
+                startScaleY = savedScaleY;
+            }
+        }
+
+        Animator resultAnimator;
+        Animator horizontalAnimator = ObjectAnimator.ofFloat(view, View.SCALE_X, startScaleX, endScaleX);
+        Animator verticalAnimator = ObjectAnimator.ofFloat(view, View.SCALE_Y, startScaleY, endScaleY);
+        if (scaleHorizontalEnable && scaleVerticalEnable) {
+            view.setScaleX(startScaleX);
+            view.setScaleY(startScaleY);
+            resultAnimator = TransitionUtils.mergeAnimators(horizontalAnimator, verticalAnimator);
+        } else if (scaleHorizontalEnable) {
+            view.setScaleX(startScaleX);
+            resultAnimator = horizontalAnimator;
+        } else {
+            view.setScaleY(startScaleY);
+            resultAnimator = verticalAnimator;
+        }
+        addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionEnd(Transition transition) {
+//                if (scaleHorizontalEnable) {
+                view.setScaleX(initialScaleX);
+//                }
+//                if (scaleVerticalEnable) {
+                view.setScaleY(initialScaleY);
+//                }
+                transition.removeListener(this);
+            }
+        });
+        return resultAnimator;
+    }
+
+    @Override
+    public Animator onAppear(ViewGroup sceneRoot, final View view, TransitionValues startValues,
+                             TransitionValues endValues) {
+        return createAnimation(view, mDisappearedScale, 1f, startValues);
+    }
+
+    @Override
+    public Animator onDisappear(ViewGroup sceneRoot, final View view, TransitionValues startValues,
+                                TransitionValues endValues) {
+        return createAnimation(view, 1f, mDisappearedScale, startValues);
+    }
+}
